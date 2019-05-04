b08a2e7e27fe8c28c08b84dc21568edffbb55a9b
Anthony Rurup <tcrurup@gmail.com>
2019-05-04 20:12:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -16,6 +16,18 @@ class Scraper
     end
   end
   
+  def get_page
+    #grabs the entire HTML doc
+  end
+  
+  def get_courses
+    #will use a css selectos to grab all the html elements that contain a course
+    #Will be a collection of XML elements
+  end
+  
+  def make_courses
+    
+  end
 end
 
 
